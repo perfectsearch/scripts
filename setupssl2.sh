f72a56056c1279bcf172598516fc9be7274110f7
Jacob Wlliams <ponyboy47@gmail.com>
2016-05-27 09:28:41 -0600

    Added GPL License

--- setupssl2.sh
+++ setupssl2.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+# This Program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; version 2 of the License.
+# 
+# This Program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# this Program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place, Suite 330, Boston, MA 02111-1307 USA.
+# 
+# Forked from:     https://github.com/richm/scripts
+# Original script: https://github.com/richm/scripts/blob/master/setupssl2.sh
+
 if [ "$1" -a -d "$1" ] ; then
     secdir="$1"
     echo "Using $1 as sec directory"
